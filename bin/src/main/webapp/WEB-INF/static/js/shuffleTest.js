c31109bc834f9b08d4cb2a4a34534a4f980b0e47
JC <JC@147.138.84.173>
2018-12-12 17:46:02 -0500

    made new file to play music and shuffle

--- bin/src/main/webapp/WEB-INF/static/js/shuffleTest.js
+++ bin/src/main/webapp/WEB-INF/static/js/shuffleTest.js
@@ -1,45 +1,82 @@
 function run(){
-	var title = "";
-	alert("in function");
-	for(i = 0; i < 10; i++){
-		title = pickNext();
-		//alert("Title:  " + title.toString());
-		/*alert("title: " + misery[0]);
+	var songName = "";
+	do{
+	//alert("in function");
+	//for(i = 0; i < 10; i++){
+		songName = pickNext();
+	}while(songName == false);
+		//alert("songName:  " + songName.toString());
+		/*alert("songName: " + misery[0]);
 		alert("Counter " + misery[1]);*/
-		if(title.toString() == "Misery"){
-			alert(title +"\n Counter:  " + misery[1] + "\n Average" + average.toString());
+		if(songName.toString() == "Misery"){
+			alert(songName +"\n Counter:  " + misery[1] + "\n Average" + average.toString());
+			document.getElementById('source').src = "../muc/01 - Misery.mp3";
+			document.getElementById('audio').load();
+			return "";
 		}
 		
-		if(title.toString() == "Gunpowder & Lead"){
-			alert(title +"\n Counter:  " + gun[1] + "\n Average" + average.toString());
+		if(songName.toString() == "Gunpowder & Lead"){
+			alert(songName +"\n Counter:  " + gun[1] + "\n Average" + average.toString());
+			document.getElementById('source').src = "../muc/01 - Gunpowder & Lead.mp3";
+			document.getElementById('audio').load();
+			return "";
 		}
 		
-		if(title.toString() == "Love Song"){
-			alert(title +"\n Counter:  " + love[1] + "\n Average" + average.toString());
+		if(songName.toString() == "Love Song"){
+			alert(songName +"\n Counter:  " + love[1] + "\n Average" + average.toString());
+			document.getElementById('source').src = "../muc/01 - Love Song.mp3";
+			document.getElementById('audio').load();
+			return "";
 		}
 	
-		if(title.toString() == "Taos"){
-			alert(title +"\n Counter:  " + taos[1] + "\n Average" + average.toString());
+		if(songName.toString() == "Taos"){
+			alert(songName +"\n Counter:  " + taos[1] + "\n Average" + average.toString());
+			document.getElementById('source').src = "../muc/09 - Taos.mp3";
+			document.getElementById('audio').load();
+			return "";
 		}
 		
-		if(title.toString() == "Move It"){
-			alert(title +"\n Counter:  " + move[1] + "\n Average" + average.toString());
+		if(songName.toString() == "Move It"){
+			alert(songName +"\n Counter:  " + move[1] + "\n Average" + average.toString());
+			document.getElementById('source').src = "../muc/07 - Move It (Radioclit Remix).mp3";
+			document.getElementById('audio').load();
+			return "";
 		}
 	
-		if(title.toString() == "Quando, Quando, Quando"){
-			alert(title +"\n Counter:  " + quando[1] + "\n Average" + average.toString());
+		if(songName.toString() == "Quando, Quando, Quando"){
+			alert(songName +"\n Counter:  " + quando[1] + "\n Average" + average.toString());
+			document.getElementById('source').src = "../muc/04 - Quando, Quando, Quando.mp3";
+			document.getElementById('audio').load();
+			return "";
 		}
 		
-		if(title.toString() == "Hey, Soul Sister"){
-			alert(title +"\n Counter:  " + sister[1] + "\n Average" + average.toString());
+		if(songName.toString() == "Hey, Soul Sister"){
+			alert(songName +"\n Counter:  " + sister[1] + "\n Average" + average.toString());
+			document.getElementById('source').src = "../muc/02 - Hey, Soul Sister.mp3";
+			document.getElementById('audio').load();
+			return "";
 		}
 	
-		if(title.toString() == "So What"){
-			alert(title +"\n Counter:  " + what[1] + "\n Average" + average.toString());
+		if(songName.toString() == "So What"){
+			alert(songName +"\n Counter:  " + what[1] + "\n Average" + average.toString());
+			document.getElementById('source').src = "../muc/11 - So What [Explicit].mp3";
+			document.getElementById('audio').load();
+			return "";
 		}
 	
-		if(title.toString() == "Get the Party Started"){
-			alert(title +"\n Counter:  " + party[1] + "\n Average" + average.toString());
+		if(songName.toString() == "Get the Party Started"){
+			alert(songName +"\n Counter:  " + party[1] + "\n Average" + average.toString());
+			document.getElementById('source').src = "../muc/01 - Get the Party Started.mp3";
+			document.getElementById('audio').load();
+			return "";
 		}
-	}
 }
+
+function test() {
+	var source = document.getElementById('source')
+	alert(source.src);
+	source.src = "../muc/01 - Misery.mp3";
+	alert(source.src);
+	document.getElementById('audio').load();
+}
+//}
